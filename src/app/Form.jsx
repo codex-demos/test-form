9b43eab2004817918b23835ae36ff1e09eb573a8
Lewis Benson <tm.lewis.benson@gmail.com>
2024-05-15 22:15:01 -0400

    add form

---
+++ src/app/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Form() {
+  return (
+    <form method="POST" netlify name="hello">
+      <label>
+        <input type="text" name="name" />
+      </label>
+    </form>
+  );
+}
